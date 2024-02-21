497644dae519df55cf00b479b5e8ca300063fc44
Yushiroh <Yushiroh.13@gmail.com>
2024-02-22 04:06:00 +0800

    Saving before trying a different code

--- sandboxCodes/arduSerialComs/src/main.cpp
+++ sandboxCodes/arduSerialComs/src/main.cpp
@@ -3,6 +3,8 @@
 const int longSig = 2;
 const int shortSig = 3;
 
+String msg;
+
 void setup() {
 
   Serial.begin(9600);
@@ -11,15 +13,33 @@ void setup() {
 
 }
 
-void loop() {
+void loop(){ 
 
-  digitalWrite(longSig, HIGH);
-  digitalWrite(shortSig, HIGH);
-  delay(1000);
-  digitalWrite(longSig, LOW);
-  digitalWrite(shortSig, LOW);
-  delay(1000);
-  
-  
+  // digitalWrite(longSig, HIGH);
+  // digitalWrite(shortSig, HIGH);
+  // digitalWrite(longSig, LOW);
+  // digitalWrite(shortSig, LOW);
 
+  while(Serial.available()){
+    delay(50);
+    char testing = Serial.read();
+    msg += testing;
+    Serial.println(msg);
+  }
+
+  if(msg.length() > 0){
+    
+
+    if(msg == "GAGO"){
+      digitalWrite(longSig,HIGH);
+      Serial.println("TAMA");
+    }else{
+      digitalWrite(longSig,LOW);
+      Serial.println("MALI");
+    }
+
+    msg = "";
+  }
+
+  
 }
